412dadf846304d1cc92588f22a300a430224741a
Rafael Mota <rma7@cin.ufpe.br>
2018-10-12 16:57:17 -0300

    Construtor de camera

--- Camera/Camera.cpp
+++ Camera/Camera.cpp
@@ -1,6 +1,12 @@
 Camera::Camera(Vec3 position, Vec3 target, Vec3 up, double fov, double near){
 	this->fov = fov;
 	this->near = near;
+	this->position = position;
+
+	Vec3 diference = position - target;
+	this->axisZ = diference.normalize();
+	this->axisX = this->axisZ.crossProd(up).normalize();
+	this->axisZ = this->axisZ.crossProd(this->axisX).normalize();
 }
 
 Ray getRay(double x, double y, int width, int height) {
